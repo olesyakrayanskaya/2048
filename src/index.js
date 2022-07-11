e2e58ad595344c3b2007c69099f3950c85ac6676
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2022-07-11 19:52:47 +0300

    generate random element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,86 @@
 import './styles.css'
 
-console.log('help')
+const gameField = document.getElementById('game__field')
+const gameSize = 5
+const elements = []
+
+function addElement() {
+
+    const newFieldItem = document.createElement('div')
+    newFieldItem.className = 'field__item'
+    newFieldItem.innerHTML = ''
+
+    gameField.appendChild(newFieldItem)
+
+    return newFieldItem
+}
+
+for (let i = 0; i < gameSize; i++) {
+    elements[i] = []
+    for (let j = 0; j < gameSize; j++) {
+        elements[i][j] = addElement()
+    }
+}
+
+document.addEventListener('keydown', function (event) {
+
+    switch (event.code) {
+
+        case 'ArrowUp':
+            console.log('up');
+            break;
+        case 'ArrowDown':
+            console.log('down');
+            break;
+        case 'ArrowRight':
+            console.log('right');
+            break;
+        case 'ArrowLeft':
+            console.log('left');
+            break;
+
+        default:
+            return;
+
+    }
+
+    generateNewItem()
+})
+
+function generateNewItem() {
+
+    let count = 0
+
+    for (let i = 0; i < gameSize; i++) {
+        for (let j = 0; j < gameSize; j++) {
+            if (elements[i][j].getInnerHTML() == '') {
+                count++
+            }
+        }
+    }
+
+    let rundomPosition = getRandomInt(0, count)
+    const rundomNumb = getRandomInt(0, 10)
+    const rundomItemInner = rundomNumb == 0 ? 4 : 2
+
+    for (let i = 0; i < gameSize; i++) {
+        for (let j = 0; j < gameSize; j++) {
+            if (elements[i][j].getInnerHTML() == '') {
+                if (rundomPosition == 0) {
+                    elements[i][j].innerHTML = rundomItemInner
+                    return
+                }
+                rundomPosition--
+            }
+        }
+    }
+
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
