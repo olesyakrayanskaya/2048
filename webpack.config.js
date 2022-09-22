1ba7d97dc0b01ab6ce1bfcc3aa1d89bd46e272f2
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2022-09-22 14:30:56 +0300

    gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ghpages = require('gh-pages');
+
+ghpages.publish('dist', function (err) { });
 
 module.exports = {
 
